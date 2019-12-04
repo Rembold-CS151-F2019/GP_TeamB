5eec407ec272cef6aa7b80ef38acd50c25d579f7
Liandrach <54556787+Liandrach@users.noreply.github.com>
2019-12-03 17:02:26 -0800

    Updated Player Code (12/3)
    Added collision detection & removed unnecessary things

--- Project.py
+++ Project.py
@@ -25,53 +25,87 @@ class Player:
         elif key == "Right" or key == "d":
             self.player.move(5,0)
             
-    def get_player_xposition(self):
-        x = Player.getCenter().getX()
-        return x
+    def collision_detection(self,image):
         
-    def get_player_yposition(self):
-        y = Player.getCenter().getY()
-        return y
+        x1 = self.player.getP1.getX() 
+        y1 = self.player.getP1.getY()
+        #upper left coordinates (x1,y2)
+        x2 = self.player.getP2.getX()
+        y2 = self.player.getP2.getY()
+        #lower right coordinates (x2,y2)
+        #upper right coordinates (x2,y1)
+        #lower left coordinates (x1,y2)
+        
+        all_coordinates = [[x1,y1],[x2,y2],[x2,y1],[x1,y2]]
+        
+        for coordinate in all_coordinates:
+            x = coordinate[0]
+            y = coordinate[1]
+            colors = image.getPixel(x,y)
+            #will return a list of numbers for each coordinate color
+            if colors[0] < 10 and colors[1] < 10 and colors[2] < 10:
+            #less than ten for all elements = color black
+                return True
+            else:
+                return False 
+            #this will work in a for loop
+            #because if at any point the if statement isn't true, it'll break out
 
 
+        #what do I want it to do if it does detect a collision?
+            #probably stop and wait for a mouseclick or something
+        #i need an example gif 
+        
+    def end_spot(self):
+        
+        #end_spot = g.Rectangle(g.Point(600,325),g.Point(700,425))
+        
+        x1 = self.player.getP1.getX() 
+        y1 = self.player.getP1.getY()
+        x2 = self.player.getP2.getX()
+        y2 = self.player.getP2.getY()
+        
+        p1 = [x1,y1]
+        p2 = [x2,y2]
+        #p1 = upper left coordinate
+        #p2 = lower right coordinate
+        
+        if 600 < p1[0] < 700 and 325 < p1[1] < 425 and 600 < p2[0] < 700 and 325 < p2[1] < 425:
+            return True
+        else:
+            return False 
+       
 if __name__== "__main__":
 
     w = g.GraphWin("Game",750,1000)
     w.setBackground("black")
     
-    start_spot = g.Rectangle(g.Point(75,350),g.Point(125,400))
-    #start spot center at 100 by 375
-    start_spot.setFill("grey")
-    start_spot.draw(w)
-    #spawn point
-    
-    end_spot = g.Rectangle(g.Point(600,325),g.Point(700,425))
-    #end spot center at 650 by 375
-    #end spot slightly bigger than player
-    end_spot.setFill("black")
-    end_spot.draw(w)
-    
     Player = Player(w,100,375)
-    x = Player.get_player_xposition
-    y = Player.get_player_yposition
-    #should these be stored or what? how will this interact with hayden's maze
-    #absolutely noooooooooooo idea 
-        
+    
+    
+    #start spot at 100 by 375
+    #end spot at 650 by 375 
+           
+    #maze = g.Image(g.Point(375,500),"Maze.gif")
+    #how to actually draw it in the window? 
+       
     key = None
     while key != "q":
         key = w.checkKey()
         Player.control()
+        #Player.collision_detection(Maze)
     w.close()   
+
+#check corners to see if they're black (wall color) (0,0,0)
+#getPixel(x,y)
+#setPixel(x,y,Color)
+'''
+create gif same size as window with black walls 
+plop the image into the window (theoretically the same size so coordinates match)
+if any of my four corners hits something black - signify end of game
+
+'''
     
     
     
-    
-    #while 480 > x and x > 520 and y < 355 and y > 395:
-       #key = w.checkKey()
-    #Player.control()
-    #w.getMouse()
-    #w.close()
-    
-    #need to change while condition 
-    #when the player is inside the endpoint - get mouse and then end (or something)     
     
